812ce621efd88493dbc0916704ce6a2b3bdcb45d
lihuiruo110 <hsfreshlight@gmail.com>
2025-05-18 13:43:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='random_maze_game',
-    version='0.1',
-    description='Terminal-based infinite random maze game with traps and scoring',
-    author='lihuiruo110',
-    py_modules=['maze_game'],
+    name="random_maze_game",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
     entry_points={
-        'console_scripts': [
-            'maze-game = maze_game:main',
-        ],
+        "console_scripts": [
+            "maze-game = maze_game.main:start_game"
+        ]
     },
+    author="lihuiruo110",
+    description="터미널 기반 랜덤 미로 게임 (curses)",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
